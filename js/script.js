7447093937655b510eab740c8216dbd1d15e2ce9
Anderson Duarte <andersonduarte997@gmail.com>
2023-01-07 20:48:30 -0300

    3º Versão - Com a função enviar pedido para o WhatsApp implementada

--- js/script.js
+++ js/script.js
@@ -29,7 +29,9 @@ function selecionarSobremesa(botaoSelecionado){
     botaoSelecionado.classList.add("selecionado");
     AtivarBotaoFinalizar();
 }
+
 let botaoAtivo = undefined;
+
 function AtivarBotaoFinalizar(){
 
     const ativarBotao = document.querySelector("footer button");
@@ -49,8 +51,27 @@ function AtivarBotaoFinalizar(){
     /*Quando os três botões forem !== null o botão inferior deve mudar*/
 }
 function FecharPedido(){
+
+    let ultimoSele1 = document.querySelector(".secao1 .selecionado .preco");
+    let ultimoSele2 = document.querySelector(".secao2 .selecionado .preco");
+    let ultimoSele3 = document.querySelector(".secao3 .selecionado .preco");
+
     if(botaoAtivo===1){
         const fecharPedido = document.querySelector("footer button");
-        alert("Botao clicável");
+
+        const v1 = parseFloat(ultimoSele1.innerHTML.replace(',','.'));
+        const v2 = parseFloat(ultimoSele2.innerHTML.replace(',','.'));
+        const v3 = parseFloat(ultimoSele3.innerHTML.replace(',','.'));
+        const soma = (v1+v2+v3).toFixed(2);
+
+        const prato = document.querySelector(".secao1 .selecionado .nome").innerHTML;
+        const bebida = document.querySelector(".secao2 .selecionado .nome").innerHTML;
+        const sobremesa = document.querySelector(".secao3 .selecionado .nome").innerHTML;
+
+        let mensagem = `Olá, gostaria de fazer o pedido:${"\n"}- Prato: ${prato}${"\n"}- Bebida: ${bebida}${"\n"}- Sobremesa: ${sobremesa}${"\n"}Total: R$ ${soma}`;
+        alert(mensagem);
+
+        mensagem = window.encodeURIComponent(mensagem);
+        window.open(`https://wa.me/558881530433?text=${mensagem}`);
     }
 }
